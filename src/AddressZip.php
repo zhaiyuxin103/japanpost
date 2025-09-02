8c4530d6fdd33b0f70edf7ea24fc1749c09ce1c1
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-09-02 10:41:41 +0800

    feat(addresses): 从地址检索邮政编码

---
+++ src/AddressZip.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yuxin\Japanpost;
+
+use GuzzleHttp\Client;
+
+class AddressZip
+{
+    public array $guzzleOptions = [];
+
+    public function __construct(
+        protected ?string $clientId = null,
+        protected ?string $secretKey = null,
+        protected ?string $token = null
+    ) {
+        //
+    }
+
+    public function getHttpClient(): Client
+    {
+        return new Client(array_merge($this->guzzleOptions, [
+            'base_uri' => 'https://stub-qz73x.da.pf.japanpost.jp/',
+        ]));
+    }
+
+    public function setGuzzleOptions(array $options): void
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function search(
+        array $data,
+        int $page = 1,
+        int $limit = 1000,
+    ) {
+        $token    = $this->token ?: (new Token($this->clientId, $this->secretKey))->getToken();
+        $response = json_decode($this->getHttpClient()->post('api/v1/addresszip', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+            'form_params' => array_merge($data, [
+                'page'  => $page,
+                'limit' => $limit,
+            ]),
+        ])->getBody()->getContents(), true);
+
+        return $response['addresses'];
+    }
+}
